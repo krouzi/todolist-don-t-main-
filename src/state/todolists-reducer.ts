a96c41072f5125fe0b13beb426240a96f4746bf9
krouzi <93330245+krouzi@users.noreply.github.com>
2022-11-24 17:45:39 +0300

    todolists(don't mane) commit

---
+++ src/state/todolists-reducer.ts
@@ -0,0 +1,33 @@
+export type StateType = {
+    age: number
+    childrenCount: number
+    name: string
+}
+
+export type ActionType = {
+    type: string
+    [key: string]: any
+}
+
+export const userReducer = (state: StateType, action: ActionType):StateType => {
+    switch (action.type) {
+        case 'INCREMENT-AGE' :
+            return {
+                ...state,
+                age: state.age + 1
+            }
+        case 'INCREMENT-CHILDREN-COUNT':
+            /* state.childrenCount = state.childrenCount + 1*/
+            return {
+                ...state,
+                childrenCount: state.childrenCount + 1
+            }
+        case 'CHANGE-NAME':
+            return {
+                ...state,
+                name: action.newName
+            }
+        default:
+            throw new Error("I don't understand you")
+    }
+}
